9d3ee80ff4721393d450a9131142e182fc5b65c4
Jinyoung <roghabo@gmail.com>
2020-11-07 16:08:50 +0900

    set apploading

--- App.js
+++ App.js
@@ -1,6 +1,42 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Text, Image, View } from "react-native";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) =>
+  fonts.map((font) => [Font.loadAsync(font), Font.loadAsync(font)]);
 
 export default function App() {
-  return <AppLoading />;
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1562887189-e5d078343de4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      require("./assets/splash.png"),
+    ]);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <View>
+      <Text>Ready!</Text>
+    </View>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
+  );
 }
