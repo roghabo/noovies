84e4f0ba1339a75082f7966a727ba8681a1c6495
Jinyoung <roghabo@gmail.com>
2020-11-09 23:35:50 +0900

    to video 2.6

---
+++ navigation/Tabs.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useLayoutEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import Favs from "../screens/Favs";
+
+const Tabs = createBottomTabNavigator();
+
+const getHeaderName = (route) =>
+  route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: getHeaderName(route),
+    });
+  }, [route]);
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movies" component={Movies} />
+      <Tabs.Screen name="TV" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favourites" component={Favs} />
+    </Tabs.Navigator>
+  );
+};
