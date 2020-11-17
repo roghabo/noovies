e378619d621805e9a84161846d79a053313aca0e
Jinyoung <roghabo@gmail.com>
2020-11-17 19:06:14 +0900

    to video 4.4

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
-import { Dimensions, ActivityIndicator } from "react-native";
+import { ActivityIndicator, View, Dimensions } from "react-native";
+import Slide from "../../components/Movies/Slide";
 
-const { width, height } = Dimensions.get("screen");
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
 
 const Container = styled.View`
   flex: 1;
@@ -11,32 +12,31 @@ const Container = styled.View`
   justify-content: center;
 `;
 
-const Header = styled.View`
-  width: 100%;
-  height: ${height / 3}px;
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `;
-
-const Section = styled.View`
-  background-color: red;
-  height: 100%;
-`;
-
-const Text = styled.Text``;
 
 export default ({ loading, nowPlaying }) => (
   <Container>
     {loading ? (
       <ActivityIndicator color="white" size="small" />
     ) : (
-      <Header>
+      <SliderContainer>
         <Swiper controlsEnabled={false} loop timeout={3}>
           {nowPlaying.map((movie) => (
-            <Section key={movie.id}>
-              <Text>{movie.original_title}</Text>
-            </Section>
+            <Slide
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              votes={movie.vote_average}
+              backgroundImage={movie.backdrop_path}
+              poster={movie.poster_path}
+            />
           ))}
         </Swiper>
-      </Header>
+      </SliderContainer>
     )}
   </Container>
 );
